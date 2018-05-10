35a2be5a0e485992086eb2bb020a83b279eb748a
Tarek Alaa <tarek_3laa@hotmail.com>
2018-05-10 14:54:04 +0200

    Add files via upload

---
+++ mouse and cheese/main.cpp
@@ -0,0 +1,38 @@
+#include "Shape.h"
+
+int main()
+{
+    Shape s;
+     int stime=0 ,etime=0,e_stime=0,time1=180;
+     char c;
+    stime = time(NULL);
+    do
+      {
+
+       s.drow();
+       s.input();
+       s. logic();
+       etime =time(NULL);
+       e_stime=etime-stime;
+       time1=180-e_stime;
+       cout<<"Time : "<<time1<<"  sec\n";
+       if(time1<=0)
+        s.gameOver=true;
+       // s.SetCursorPos(0,0);
+       // Sleep(200);
+      // usleep(1000000);
+
+       }
+    while ((!s.winOver&&!s.gameOver));
+    if(s.gameOver){
+        system("cls");
+        s.drowGameOver();
+        Sleep(100000);
+        }
+        else if(s.winOver){
+                system("cls");
+        s.drowWinOver();
+                Sleep(100000);
+        }
+    return 0;
+}
---
+++ mouse and cheese/src/Shape.cpp
@@ -0,0 +1,217 @@
+#include "Shape.h"
+
+Shape::Shape()
+{
+
+}
+edir dir;
+/*void Shape::SetCursorPos(int x, int y)
+{
+ printf("%c[%d;%df",0x1B,y,x);
+ }*/
+void Shape::clearscreen()
+{
+    HANDLE hOut;
+    COORD Position;
+
+    hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    Position.X = 0;
+    Position.Y = 0;
+    SetConsoleCursorPosition(hOut, Position);
+
+}
+void Shape::drowWinOver()
+{
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+
+
+    cout<<"\t  **                         **     #                     \n";
+    cout<<"\t   **                       **          **                \n";
+    cout<<"\t    **         ***         **      **   **                \n";
+    cout<<"\t     **       ** **       **       **   ***********       \n";
+    cout<<"\t      **     **   **     **        **   * ******* *       \n";
+    cout<<"\t       **   **     **   **         **   **       **       \n";
+    cout<<"\t        ** **       ** **          **   **       **       \n";
+    cout<<"\t         ***         ***           **   **       **   \n\n\n";
+
+
+    cout<<"\t                **                  **    **************               \n";
+    cout<<"\t************     **                **     * ********** *     **     ***\n";
+    cout<<"\t*          *      **              **      * *        * *      **   **  \n";
+    cout<<"\t* ******** *       **            **       * *        * *      **  **   \n";
+    cout<<"\t* *      * *        **          **        * ********** *      ** **    \n";
+    cout<<"\t* *      * *         **        **         **************      ***      \n";
+    cout<<"\t* ******** *          **      **          **                  **       \n";
+    cout<<"\t*          *           **    **           **                  **       \n";
+    cout<<"\t************            **  **            **                  **       \n";
+    cout<<"\t                         ****             **                  **       \n";
+    cout<<"\t                                          *********           **       \n";
+    cout<<"\t                                          *********                  \n\n";
+
+}
+
+void Shape::drowGameOver()
+{
+    cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<endl;
+cout<<"****"<<"\t****"<<"*   *       *  *****   %"<<endl;
+cout<<"*  *"<<"\t*  **"<<"\t*"<<"       *"<<"  *   *"<<endl;
+cout<<"*  *"<<"\t*  **"<<"\t* *"<<"   * *"<<"  *   *"<<endl;
+cout<<"****"<<"\t** **"<<"\t*   *"<<"*  *"<<"  *   *"<<endl;
+cout<<"   *"<<"\t   ****"<<"\t*       *"<<"  *****"<<endl;
+cout<<"   *"<<"                       *"<<endl;
+cout<<"****"<<"                       ******"<<endl;
+cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<endl;
+cout<<" ****"<<"  *     *"<<"  ***"<<"   *  *"<<endl;
+cout<<" *  *"<<"   *   *"<<"   * *"<<"   * *"<<endl;
+cout<<" *  *"<<"    * *"<<"    ***"<<"   **"<<endl;
+cout<<" ****     *     *     *"<<endl;
+cout<<"                ***   *           %"<<endl;
+cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<endl;
+
+}
+void Shape:: drow()
+{
+    /*in Windows OS you should use*//**system("cls");*//*but in Linux OS you should use*/ ///system("clear");
+   //s.SetCursorPos(0,0);
+   system("cls");
+    for(int y=0; y<30; y++)
+    {
+        for(int x=0; x<25; x++)
+        {
+            if(shape[y][x]=='m')
+                cout<<"@";
+            else if(shape[y][x]=='s')
+                cout<<" ";
+            else if (shape[y][x]=='w')
+                cout<<"#";
+            else if(shape[y][x]=='c')
+                cout<<"$";
+
+
+        }
+        cout<<endl;
+
+    }
+
+}
+
+void Shape::input()
+{
+    dir=STOP;
+   if(GetAsyncKeyState(VK_UP))
+            dir=UP;
+        else  if(GetAsyncKeyState(VK_DOWN))
+            dir=DOWN;
+        else  if(GetAsyncKeyState(VK_RIGHT))
+           dir=RIGHT;
+        else  if(GetAsyncKeyState(VK_LEFT))
+           dir=LEFT;
+
+         /*  if(c=='w')
+           dir=UP;
+           else if(c=='a')
+           dir=LEFT;
+           else if(c=='d')
+            dir=RIGHT;
+          else if(c=='s')
+          dir=DOWN;*/
+}
+void Shape::logic()
+{
+   b=false;
+    switch(dir)
+    {
+    case LEFT :
+
+        for(int y=0; y<30; y++)
+        {
+            for(int x=0; x<25; x++)
+            {
+                if(shape[y][x]=='m'&&shape[y][x-1]=='s'&&x!=0)
+                {
+                    swap(shape[y][x],shape[y][x-1]);
+                    b=true;
+                    break;
+                }
+                if(shape[y][x]=='m'&&shape[y][x-1]=='c'&&x!=0)
+                    winOver=true;
+
+            }
+            if(b)
+            break;
+        }
+        break;
+
+
+    case RIGHT :
+
+        for(int y=0; y<30; y++)
+        {
+            for(int x=0; x<25; x++)
+            {
+                if(shape[y][x]=='m'&&shape[y][x+1]=='s'&&x!=59)
+                {
+                    swap(shape[y][x],shape[y][x+1]);
+                    b=true;
+                    break;
+                }
+                if(shape[y][x]=='m'&&shape[y][x+1]=='c'&&x!=59)
+                    winOver=true;
+            }
+            if(b)
+            break;
+        }
+
+
+
+        break;
+
+    case UP :
+        for(int y=0; y<30; y++)
+        {
+            for(int x=0; x<25; x++)
+            {
+                if(shape[y][x]=='m'&&shape[y-1][x]=='s'&&y!=0)
+                {
+                    swap(shape[y][x],shape[y-1][x]);
+                   b=true;
+                   break;
+
+                }
+                if(shape[y][x]=='m'&&shape[y-1][x]=='c'&&y!=0)
+                    winOver=true;
+
+            }
+            if(b)
+                break;
+        }
+
+        break;
+    case DOWN :
+        for(int y=0; y<30; y++)
+        {
+            for(int x=0; x<25; x++)
+            {
+                if(shape[y][x]=='m'&&shape[y+1][x]=='s'&&y!=29)
+                {
+                    swap(shape[y][x],shape[y+1][x]);
+                    b=true;
+                    break;
+                }
+                if(shape[y][x]=='m'&&shape[y+1][x]=='c'&&y!=29)
+                    winOver=true;
+
+
+
+            }
+            if(b)
+                break;
+        }
+
+        break;
+    }
+
+}
